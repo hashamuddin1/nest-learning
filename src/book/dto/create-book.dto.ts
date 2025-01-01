580dc45bfd55bd8444d1cc2d2da0fad5f514479a
hashamuddin1 <hashamlaptop@gmail.com>
2025-01-01 23:06:06 +0500

    add is not empty at dto

--- src/book/dto/create-book.dto.ts
+++ src/book/dto/create-book.dto.ts
@@ -1,11 +1,14 @@
-import { IsString, IsInt } from 'class-validator';
+import { IsString, IsInt, IsNotEmpty } from 'class-validator';
 export class Book {
   @IsInt()
   id: number;
   @IsString()
+  @IsNotEmpty({ message: 'Title is required' })
   title: string;
   @IsString()
+  @IsNotEmpty({ message: 'Author is required' })
   author: string;
   @IsString()
+  @IsNotEmpty({ message: 'Published is required' })
   published: string;
 }
